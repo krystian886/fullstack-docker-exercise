759dc3a12a4bdb7b5b036a53d128be7d4df30aa8
Krystian Turek <krystian.turek2@gmail.com>
2021-07-26 13:24:25 +0200

    added backend connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import List from './components/List'
 import DataClass from './models/Data';
+import ListService from './services/List.service';
+
 
 
 function App() {
-  const hardcodedData = [
+  const [backendData, setBackendData] = useState([]);
+
+  useEffect( () => {
+    const axiosRequest = async () => {
+      const response = await ListService.getList();
+      setBackendData(response.data);
+    }
+    axiosRequest();
+  }, []);
+
+  const useBackendData: DataClass[] = backendData.map( (data: any) => {
+    return new DataClass(data.id, data.title, data.body);
+  })
+
+  const useHardcodedData = [
     new DataClass(1, 'aa', 'bb'),
     new DataClass(2, 'aaa', 'bbb'),
     new DataClass(3, 'aaaa', 'bbbb'),
@@ -13,7 +29,9 @@ function App() {
 
   return (
     <>
-      <List listData={hardcodedData}/>
+      <List listData={useHardcodedData}/>
+      <br/>
+      <List listData={useBackendData}/>
     </>
   );
 }
--- frontend/src/services/List.service.tsx
+++ frontend/src/services/List.service.tsx
@@ -2,12 +2,7 @@ import axios from "axios";
 
 class ListService {
     async getList(){
-        try {
-            const response = await axios.get("http://localhost:8080/api/data");
-        } catch (e) {
-            console.log(e);
-            return "";      // because of React.useEffect in UserProfile.tsx
-        }
+        return await axios.get('http://localhost:8080/api/data');
     }
 }
 
